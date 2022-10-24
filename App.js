bcd00a4ce49a46a4d8e5386f4ebbbbeace42a493
jclizav <lizav91@gmail.com>
2022-10-23 19:27:48 -0500

    oye modifca el app

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Ohols</Text>
+      <Text>Ohols gabuchu</Text>
       <StatusBar style="auto" />
     </View>
   );
